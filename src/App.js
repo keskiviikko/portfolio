421ab489d0b3466e033a0a8d23badd56041ad8ba
Rasmus Rajasalo <rarasmus@outlook.com>
2020-01-14 13:35:17 +0200

    Add header style

--- src/App.js
+++ src/App.js
@@ -1,34 +1,37 @@
-import React from 'react'
-import { BrowserRouter as Router, Switch, Route, } from 'react-router-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import Nav from 'react-bootstrap/Nav'
-import './App.css';
-import Home from './components/Home'
-import About from './components/About'
-import Projects from './components/Projects'
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Nav from "react-bootstrap/Nav";
+import "./App.css";
+import Home from "./components/Home";
+import About from "./components/About";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <Router>
       <div>
-        <h2 className='header'>Portfolio</h2>
+        <h2 className="header">Portfolio</h2>
         <Nav className="justify-content-center" activeKey="/">
           <Nav.Item>
             <Nav.Link href="/">Home</Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-1" href="/about">About</Nav.Link>
+            <Nav.Link eventKey="link-1" href="/about">
+              About
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-2" href="/projects">Projects</Nav.Link>
+            <Nav.Link eventKey="link-2" href="/projects">
+              Projects
+            </Nav.Link>
           </Nav.Item>
-
         </Nav>
         <hr />
         <Switch>
-          <Route exact path='/' component={Home} />
-          <Route path='/about' component={About} />
-          <Route path='/projects' component={Projects} />
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/projects" component={Projects} />
         </Switch>
       </div>
     </Router>
